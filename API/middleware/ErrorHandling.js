3c0963c150c86160384e99684bbfb4c167b1a5fe
azinisikhona <azizsiyalana10@gmail.com>
2023-08-31 12:20:58 +0200

    err handling

--- API/middleware/ErrorHandling.js
+++ API/middleware/ErrorHandling.js
@@ -1,1 +1,12 @@
-// 
+function errorHandling(err, req, res, next) {
+    if(err) {
+        let status = err.status || 500
+        res.json({
+            status,
+            msg: "an error occurred: please try again later"
+        })
+    }
+    next()
+}
+
+module.exports = errorHandling
